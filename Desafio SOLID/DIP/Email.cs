9d775aab673b11f4a0c48685752f54eec229222c
anglusa <otavio.baroni@etec.sp.gov.br>
2024-08-30 17:08:48 -0300

    Finalizado o Desafio SOLID

---
+++ Desafio SOLID/DIP/Email.cs
@@ -0,0 +1,13 @@
+
+namespace SOLID.DIP.Solucao
+{
+    public class Email
+    {
+        public string Endereco { get; set; }
+
+        public bool Validar()
+        {
+            return Endereco.Contains("@");
+        }
+    }
+}
